ff5c5c30d3e133fa840e2db26c6a394bd0acddda
Baneboo <mikkel_10@hotmail.com>
2023-12-20 15:56:11 +0100

    Added some more keymaps

--- lua/baneboo/core/keymaps.lua
+++ lua/baneboo/core/keymaps.lua
@@ -35,6 +35,10 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>s>", ":vertical resize +5<CR>", { desc = "Increase window width" })
+keymap.set("n", "<leader>s<", ":vertical resize -5<CR>", { desc = "Decrease window width" })
+keymap.set("n", "<leader>s+", ":resize +5<CR>", { desc = "Increase window height" })
+keymap.set("n", "<leader>s-", ":resize -5<CR>", { desc = "Decrease window height" })
 
 -- cursor stay in place when appending
 keymap.set("n", "J", "mzJ`z", { desc = "J appends the line underneath to the current line" })
