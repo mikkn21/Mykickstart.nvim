b21e4d8505499aee52439b0cb0eb53d86b5211c4
Baneboo <mikkel_10@hotmail.com>
2023-12-21 07:31:46 +0100

    Made harpoon keybinds better

--- lua/baneboo/plugins/harpoon.lua
+++ lua/baneboo/plugins/harpoon.lua
@@ -12,21 +12,21 @@ return {
 
 
 
-        keymap.set("n", "<leader>la", mark.add_file, { desc = "[L]Harpoon [A]dd file with harpoon" })
-        keymap.set("n", "<leader>le", ui.toggle_quick_menu, { desc = "[L]Harpoon [E]xplore menu" })
-        keymap.set("n", "<leader>l1", function()
+        keymap.set("n", "<leader>ma", mark.add_file, { desc = "Harpoon [M]ark [A]dd" })
+        keymap.set("n", "<leader>me", ui.toggle_quick_menu, { desc = "Harpoon [M]ark [E]xplore ui " })
+        keymap.set("n", "<leader>m1", function()
             ui.nav_file(1)
-        end, { desc = "[L]Harpoon goto mark [1]"})
-        keymap.set("n", "<leader>l2", function()
+        end, { desc = "Harpoon [M]ark goto [1]"})
+        keymap.set("n", "<leader>m2", function()
             ui.nav_file(2)
-        end, { desc = "[L]Harpoon goto mark [2]"})
-        keymap.set("n", "<leader>l3", function()
+        end, { desc = "Harpoon [M]ark goto [2]"})
+        keymap.set("n", "<leader>m3", function()
             ui.nav_file(3)
-        end, { desc = "[L]Harpoon goto mark [3]"})
-        keymap.set("n", "<leader>l4", function()
+        end, { desc = "Harpoon [M]ark goto [3]"})
+        keymap.set("n", "<leader>m4", function()
             ui.nav_file(4)
-        end, { desc = "[L]Harpoon goto mark [4]"})
-        keymap.set("n", "<leader>ln", ui.nav_next, { desc = "[L]Harpoon goto [N]ext harpoon mark" })
-        keymap.set("n", "<leader>lp", ui.nav_prev, { desc = "[L]Harpoon goto [P]revious harpoon mark" })
+        end, { desc = "Harpoon [M]ark goto [4]"})
+        keymap.set("n", "<leader>mn", ui.nav_next, { desc = "Harpoon [M]ark goto [N]ext" })
+        keymap.set("n", "<leader>mp", ui.nav_prev, { desc = "Harpoon [M]ark goto [P]revious" })
     end,
 }
